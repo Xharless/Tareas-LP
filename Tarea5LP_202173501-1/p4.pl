45675eddc220df8afd52dd42abbb30446c0b0604
Xharless <carlosarevalo1702@hotmail.com>
2023-11-13 13:21:28 -0300

    comentario pregunta 4 lista

--- Tarea5LP_202173501-1/p4.pl
+++ Tarea5LP_202173501-1/p4.pl
@@ -1,3 +1,4 @@
+%Relaciones en el grafico
 sigue(p1, p2).
 sigue(p2, p3).
 sigue(p3, p4).
@@ -21,13 +22,25 @@ sigue(p11, p12).
 sigue(p12, p13).
 sigue(p13, p14).
 
+%Determina si un nodo pertenece a la rama principal del grafo
+%Entrada:
+%      - Nodo: Nodo que se quiere verificar
+%Salida:
+%      - R: Resultado de el nodo, si es que pertenece a la rama
+%      principal o no
 principal(Nodo, R) :-
     camino(Nodo, p1, [Nodo], Camino),
     reverse(Camino, CaminoInverso),
     writeln(CaminoInverso),
     (member(p8, Camino) -> R = "Es de la rama principal" ; R = "No es de la rama principal").
 
-% Regla para encontrar un camino en el grafo
+%Encuentra el camino del grafo
+%Entrada:
+%       -NodoInicial:Nodo de inicio
+%       -NodoFinal: Nodo de destino
+%       -CaminoVisitado: Lista de nodos visitados hasta el momento
+%Salida:
+%       -Camino: Lista que representa el camino encontrado
 camino(Nodo, Nodo, Camino, Camino).
 camino(NodoInicial, NodoFinal, CaminoVisitado, Camino) :-
     sigue(NodoInicial, Nodo),
