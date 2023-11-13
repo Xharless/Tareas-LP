a35938be3fb0853eaa18fb250bcea846b3bf11d4
Xharless <carlosarevalo1702@hotmail.com>
2023-11-13 13:04:04 -0300

    Comentario pregunta 2 lista

--- Tarea5LP_202173501-1/p2.pl
+++ Tarea5LP_202173501-1/p2.pl
@@ -1,10 +1,22 @@
 % Definición de la cerradura
 cerradura(1, 4, 5, 1, 0).
 
-% Regla para calcular la diferencia absoluta
+% Calcula la diferencia absoluta entre dos numeros
+% Entradas:
+%       -X: Primer numero
+%       -Y: Segundo numero
+% Salida:
+%       -D: Diferencia absoluta entre X e Y
 diferencia(X, Y, D) :- D is abs(X - Y).
 
-% Regla para verificar la combinación
+% Verifica si una combinacion dada está correcta, si está cercana o
+% lejana de la combinacion correcta.
+%
+% Entradas:
+%        -X1, X2, X3, X4, X5: Digitos de la combinacion
+% Salida:
+%        -R: Resultado que indica si la combinacion es correcta, cercana
+%        o lejana
 verificar(X1, X2, X3, X4, X5, R) :-
     cerradura(C1, C2, C3, C4, C5),
     diferencia(X1, C1, D1),
