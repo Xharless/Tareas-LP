f1ac9a1258a49112bf35790b2bcb2a7e9493c57c
Xharless <carlosarevalo1702@hotmail.com>
2023-11-13 10:59:53 -0300

    Comentarios listos P1

--- Tarea5LP_202173501-1/p1.pl
+++ Tarea5LP_202173501-1/p1.pl
@@ -1,8 +1,17 @@
+%Definiciones de las combinaciones
 cifrado([0, 0], a).
 cifrado([0, 1], g).
 cifrado([1, 0], c).
 cifrado([1, 1], t).
 
+
+%Funcion para descifrar una lista de combinaciones de bits
+%Entradas:
+%        -Lista de combinaciones de bits: [B1, B2, B3,...]
+%
+%Salida:
+%        -Lista de letras descifradas: [Letra1, Letra2, Letra3, ...]
+%
 descifrar([], []).
 descifrar([B1, B2 | RestoMensaje], [Base | RestoRespuesta]) :-
     cifrado([B1, B2], Base),
