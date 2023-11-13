bde4a5349063e86adf06dd82d1b8813ece073928
Xharless <carlosarevalo1702@hotmail.com>
2023-11-13 13:14:25 -0300

    Comentarios pregunta 3 lista

--- Tarea5LP_202173501-1/p3.pl
+++ Tarea5LP_202173501-1/p3.pl
@@ -1,7 +1,22 @@
+%Defincion de la cerradura
 cerradura(1, 4, 5, 1, 0).
 
+%Verifica la coincidencia entre 2 numeros
+%Entradas:
+%       -X: primer numero
+%       -Y: segundo numero
+%Salida:
+%       -C: 1 si X  Y son iguales, en caso contrario es 0
 coincidencia(X, Y, C) :- (X = Y -> C = 1 ; C = 0).
 
+% Verifica la cantidad de numeros que coinciden entre una combinacion y
+% la combinacion correcta
+%
+% Entradas:
+%        -D1, D2, D3, D4, D5: Digitos de la combinacion ingresada
+% Salida:
+%        -Resultado: Indica si la combinacion es correcta o la
+%        cantidadde digitos que estan correctos
 verificar(D1, D2, D3, D4, D5, Resultado) :-
     cerradura(C1, C2, C3, C4, C5),
     coincidencia(D1, C1, C1_),
